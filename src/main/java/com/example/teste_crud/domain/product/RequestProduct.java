a9d44507366336f9a86260de489f45f9cf335a71
Carlos Eduardo <cadubr13@gmail.com>
2024-09-16 16:30:36 -0300

    feat: complete CRUD for products

---
+++ src/main/java/com/example/teste_crud/domain/product/RequestProduct.java
@@ -0,0 +1,16 @@
+package com.example.teste_crud.domain.product;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record RequestProduct(
+
+    String id,
+
+    @NotBlank
+    String name,
+
+    @NotNull
+    Integer price_in_cents
+
+) {}
